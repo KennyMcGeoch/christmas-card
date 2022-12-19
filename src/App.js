77cb8d4e1b521939a1887c3b867f5a0a607df5b7
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-12-19 12:48:18 +0000

    Added 3 snow animations in components for use with tsparticles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import snowOne from './SnowOne';
+import snowTwo from './SnowTwo';
+import snowThree from './SnowThree';
 
 function App() {
+  const particlesInit = useCallback(async engine => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+}, []);
+
+const particlesLoaded = useCallback(async container => {
+    await console.log(container);
+}, []);
+let weather = snowTwo
   return (
     <div className="App">
+      <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={weather}/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Placeholder</h1>
       </header>
     </div>
-  );
+  );  
 }
 
 export default App;
